7594906db531ebb159a85173671d5b56efb688bc
Kenan Bejtic <126878548+Kenan-Bejtic@users.noreply.github.com>
2023-05-21 18:38:16 +0200

    Admin menu i hashiranje odgovara na sq

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include "json/single_include/nlohmann/json.hpp"
 #include <openssl/evp.h>
 #include <openssl/rand.h>
-
 using namespace std;
 using json = nlohmann::json;
 
@@ -48,10 +47,23 @@ bool provjeraKorisnickogImena(const string& korisnickoIme, const string& file)
     infile.close();
     return false;
 }
+//Funckija koja provjerava da li email ima @ 
+bool provjeraZnaka (const string email){
+
+	for(int i=0;i<email.length();i++){
+		if(email[i]=='@'){
+			return true;
+		}
+	}	
+return false;
+    
+    
+}
+
 
 //Funkcija koja provjerava da li email vec postoji
 bool provjeraEmail(const string& email, const string& file)
-{
+{	
 	ifstream infile(file);
     json jsonNiz;
     
@@ -65,7 +77,7 @@ bool provjeraEmail(const string& email, const string& file)
         if(jsonObj["email"] == email)	
         	return true;
     }
-    
+
     infile.close();
     return false;
 }
@@ -97,10 +109,11 @@ void unos() {
 	// Unosimo podatke 
 	Podaci p;
     json jsonObj;
-    unsigned char salt[8];
+    int duzinaSifre=0;
+	unsigned char salt[8];
     int salt_len = 8;
     cout << "Unesite korisnicko ime : ";
-
+	
     getline(cin, p.korisnickoIme);
     //Ponavlja se unos ako se unese vec postojece korisnicko ime
     if (provjeraKorisnickogImena(p.korisnickoIme, file))
@@ -112,23 +125,36 @@ void unos() {
 	//Ponavlja se unos ako se unese vec postojeci email
     cout << "Unesite email: ";
     getline(cin, p.email);
+    
+    
+    if (!provjeraZnaka(p.email)){
+		do{
+			cout <<"Vas mail nema @ znak, pokusajte ponovo: ";
+			getline(cin,p.email);
+		}while(!provjeraZnaka(p.email));
+	}
+	
+    
+    
     if (provjeraEmail(p.email, file))
         do {
-            cout << "Email vec postoji, unesite ponovo: ";
+            cout << "Email vec postoji, unesite ponovo (vas mail mora da sadrzi @ znak): ";
             getline(cin, p.email);
-        } while (provjeraEmail(p.email, file));
+        } while (provjeraEmail(p.email, file) || !provjeraZnaka(p.email) );
 
 	//Predstavljanje sifre preko zvjezdica
-    cout << "Unesite sifru profila: ";
+    cout << "Unesite sifru profila (minimum 8 karaktera): ";
     char c;
-    while ((c = _getch()) != '\r') { // \r je Enter 
+    while ((c = _getch()) != '\r' || duzinaSifre<8) { // \r je Enter 
         if (c == '\b' && !p.sifra.empty()) { // brisanje
             p.sifra.pop_back();
             cout << "\b \b";
+            duzinaSifre--;
         }
         else if (isprint(c)) {
             p.sifra += c;
             cout << '*';
+            duzinaSifre++;
         }
     }
     cout << endl;
@@ -167,13 +193,15 @@ void unos() {
 
     generisanjeSoli(p.salt, salt_len); 
     string hashiranaSifra = hashSifre(p.sifra, p.salt, salt_len);
+
+	string hashiranOdgovor=hashSifre(p.odgovor,p.salt,salt_len);
     
 	//Pisanje u jedan json objekat
     jsonObj["korisnickoIme"] = p.korisnickoIme;
     jsonObj["email"] = p.email;
     jsonObj["sifra"] = hashiranaSifra;
     jsonObj["sigurnosnoPitanje"] = sigurnosnaPitanja[p.sigurnosnoPitanje - 1];
-    jsonObj["odgovor"] = p.odgovor;
+    jsonObj["odgovor"] = hashiranOdgovor;
     jsonObj["salt"] = p.salt;
 
 	//dodavanje json objekta na kraj json niza
@@ -230,12 +258,15 @@ void ispisProvjera(){
     infile.close();
 }
 
+
 //Funkcija za promjenu sifre
 void promjenaSifre(){
 	string temp;
 	string odg;
 	string novaS;
-	
+	string hashOdgovor;
+	int duzinaSifre=0;
+
 	ifstream infile(file);
     json jsonNiz;
 	
@@ -245,7 +276,6 @@ void promjenaSifre(){
     
 	
 	cout << "Unesite korisnicko ime: ";
-	//cin.ignore();
 	getline(cin,temp);
 	
 	if (provjeraKorisnickogImena(temp,file)){ // Provjeravamo da li postoji osoba sa tim korisnickim imenom
@@ -253,28 +283,37 @@ void promjenaSifre(){
 	    int salt_len = 8;
 		for (auto& jsonObj : jsonNiz){ // prolazimo kroz niz osoba i trazimo isto korisnicko ime
 			if(temp==jsonObj["korisnickoIme"]){
-				do {
-					cout << "Sigurnosno pitanje: " << jsonObj["sigurnosnoPitanje"] << endl;
-					cout << "Odgovor: ";
+				for (int i = 0; i < salt_len; ++i) {
+				    salt[i] = jsonObj["salt"][i].get<unsigned char>();
+				}
+				
+				do{	
+					cout << "Unesite odgovor za sigurnosno pitanje! " <<endl;
 					getline(cin,odg);
-				}while(jsonObj["odgovor"]!=odg); // trazimo odgovor na sigurnosno pitanje 
-				cout << "Unesite novu sifru profila: ";
+					hashOdgovor=hashSifre(odg,salt,salt_len);
+				}while(jsonObj["odgovor"]!=hashOdgovor);
+				
+				cout << "Unesite novu sifru profila (minimum 8 karaktera): ";
 				char c;
-				while ((c = _getch()) != '\r') { // \r je Enter 
+				while ((c = _getch()) != '\r' || duzinaSifre<8) { // \r je Enter 
 	    			if (c == '\b' && !novaS.empty()) { // brisanje
 						novaS.pop_back();
 			        	cout << "\b \b";
+			        	duzinaSifre--;
 					}
 					else if (isprint(c)) {
 						novaS += c;
 			        	cout << '*';
+			        	duzinaSifre++;
 					}
 				}	
 			
 			generisanjeSoli(salt, salt_len);
 	    	string hashiranaSifra = hashSifre(novaS, salt, salt_len);
+	    	string novihashOdg=hashSifre(odg,salt,salt_len);
 			jsonObj["sifra"]=hashiranaSifra;
-			jsonObj["salt"] = salt; // unos nove sifre
+			jsonObj["odgovor"]=novihashOdg;
+			jsonObj["salt"] = salt; 
 			ofstream outfile(file);
 			
 			cout<<endl;
@@ -284,9 +323,11 @@ void promjenaSifre(){
 			return;
 			}
 		}
-	}
+
+}
 	cout << "Nismo pronasli korisnika!"<<endl;
 }
+
 
 
 //Funkcija za ulaz u sistem
@@ -308,8 +349,6 @@ void login(){
 	if (provjeraKorisnickogImena(username,file)){ //Provjerava da li postoji korinsik sa tim korisnickim imenom
 		for (auto& jsonObj : jsonNiz){ // prolazi kroz niz
 			if(username==jsonObj["korisnickoIme"]){ // staje na osobi koja ima isto korisnicko ime
-			
-				// Uzimanje salt-a iz json-a
 				for (int i = 0; i < salt_len; ++i) {
 				    salt[i] = jsonObj["salt"][i].get<unsigned char>();
 				}
@@ -349,19 +388,128 @@ void login(){
 	}
 }
 
+void brisanjeKorisnika(){
+	
+	ifstream infile(file);
+    json jsonNiz;
+     
+    if (infile.good()) {
+        infile >> jsonNiz;
+    }
+    int index=0;
+	string username;
+	cout << "Unesite korisnicko ime za brisanje: ";
+	getline(cin,username);
+	if(provjeraKorisnickogImena(username,file)){
+		for (auto& jsonObj : jsonNiz){index++; 
+			if(username==jsonObj["korisnickoIme"]){	
+				jsonNiz.erase(index-1);
+				ofstream outfile(file);
+				cout<<endl;
+				outfile << jsonNiz.dump(4) << endl; 
+		    	outfile.close();
+		    	return;
+			}
+		}
+	}
+	else{
+		cout << "Osoba ne postoji sa tim korisnickim imenom! "<<endl;
+	}
+	
+}
+
+void ispisKorisnika(){
+	
+	ifstream infile(file);
+    json jsonNiz;
+     
+    if (infile.good()) {
+        infile >> jsonNiz;
+    }
+    
+    cout<<"Unesite korisnicko ime osobe cije podatke zelite ispisati: ";
+    string username;
+    getline(cin,username);
+    
+    if(provjeraKorisnickogImena(username,file)){
+    	for(auto &jsonObj:jsonNiz){
+    		if(jsonObj["korisnickoIme"]==username){
+    			cout << "Korisnicko ime: " << jsonObj["korisnickoIme"] << endl;
+        		cout << "Email: " << jsonObj["email"] << endl;
+        		cout<< "Sifra: ";
+        		string sifra=jsonObj["sifra"];
+				for(int i=0;i<sifra.length();i++){
+        			cout<<"*";
+				}
+				cout<<endl;
+        		cout << "Sigurnosno pitanje: " << jsonObj["sigurnosnoPitanje"] << endl;
+        		cout << "Odgovor: " << jsonObj["odgovor"] <<endl;
+        		cout << endl;
+        		infile.close();
+        		return;
+			}
+		}
+	}
+	else{
+		cout << "Nismo pronasli korisnika! ";
+	}
+    	
+	
+	
+}
+
+
+void adminMenu(){
+	int k;	
+	bool fail;
+	do {  	
+		system("cls");
+		cout<<"1. Ispis svih profila "<<endl;
+		cout<<"2. Provjera ispisa "<<endl;
+		cout<<"3. Brisanje profila " <<endl;
+		cout<<"4. Pretraga po korisnickom imenu"<<endl;
+		cout<<"0. Izlaz"<<endl;
+		cout<<endl;
+			do{
+			
+	      		cout<<"Izaberite opciju: "<<endl; 
+	      		cin >> k;  
+				fail = cin.fail();  
+				cin.clear();  
+	      		cin.ignore(INT_MAX, '\n');  
+  			}while(fail);
+		if (k==0){
+			return;
+		}
+		if(k==1){
+			ispis();
+		}
+		if(k==2){
+			ispisProvjera();
+			
+		}
+		if (k==3){
+			brisanjeKorisnika();
+		}	
+		if(k==4){
+			ispisKorisnika();
+		}
+		system("pause");
+		system("cls");	
+	
+	}while(true);
+}
 
 int main(){
 
 	int a;
 	bool fail;
-	//Menu
 	do{
 		cout<<"0. Izlaz"<<endl;
 		cout<<"1. Unos profila "<<endl;
-		cout<<"2. Ispis svih profila "<<endl;
-		cout<<"3. Provjera ispisa "<<endl;
-		cout<<"4. Promjena sifre " <<endl;
-		cout<<"5. Login"<<endl;
+		cout<<"2. Promjena sifre " <<endl;
+		cout<<"3. Login"<<endl;
+		cout<<"4. Admin"<<endl;
 		cout<<endl;
 		do  
    		{  
@@ -378,23 +526,16 @@ int main(){
 		}
 		if(a==1){
 			unos();
-			
 		}
 		if(a==2){
-			ispis();
+			promjenaSifre();
 			
 		}
 		if(a==3){
-			ispisProvjera();
-			
+			login();			
 		}
 		if (a==4){
-			promjenaSifre();
-	
-		}
-		if (a==5){
-			login();
-	
+			adminMenu();
 		}
 		system("pause");
 		system("cls");
